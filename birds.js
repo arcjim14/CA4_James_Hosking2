1cbc5697fb9da73c31e4ad02be5c8ad79c19be3e
arcjim14 <87861300+arcjim14@users.noreply.github.com>
2021-08-13 11:42:32 +0800

    Add files via upload

---
+++ birds.js
@@ -0,0 +1,23 @@
+const buttons = Array.from(document.querySelectorAll('button'));
+const contentBlocks = Array.from(document.querySelectorAll('section'))
+
+function hide_sections(arr){
+    arr.forEach(a => {
+        a.classlist.add('hidden');
+    });
+}
+
+function show_section(index, sections) {
+    if(index !== undefined && sections !== undefined) {
+        hide_sections(sections);
+        sections[index].classlist.remove('hidden');
+    }
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const contentBlocks = Array.from(document.querySelectorAll('section'));
+        const index = button.getAttribute('data-index');
+
+    });
+});
